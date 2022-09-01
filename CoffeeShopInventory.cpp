9543889987f4eee7637354a820d1c8ca07d062e3
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-09-01 08:22:20 -0600

    initial commit.

---
+++ CoffeeShopInventory.cpp
@@ -0,0 +1,19 @@
+/*---------------------------------------------------------------------------
+Copyright (C), 2022-2023, Sara Echeverria (bl33h)
+@author Sara Echeverria
+FileName: CoffeeShopInventory
+@version: I
+Creation: 01/09/2022
+Last modification: 01/09/2022
+------------------------------------------------------------------------------*/
+// Imports
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <iostream>
+#include <math.h>
+#include <errno.h>
+
+// Namespace
+using namespace std;
