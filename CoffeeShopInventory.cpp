83c59973f6fdda729f103e738004b59fd47093fa
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-09-06 14:30:15 -0600

    bugs fixed.

--- CoffeeShopInventory.cpp
+++ CoffeeShopInventory.cpp
@@ -4,7 +4,7 @@ Copyright (C), 2022-2023, Sara Echeverria (bl33h)
 FileName: CoffeeShopInventory
 @version: I
 Creation: 01/09/2022
-Last modification: 01/09/2022
+Last modification: 06/09/2022
 ------------------------------------------------------------------------------*/
 // Imports
 #include <stdlib.h>
